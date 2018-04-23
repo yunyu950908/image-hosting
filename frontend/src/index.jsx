f144c6578ad04549fb1911c55b4a3b105f9cd924
yunyu950908 <yunyu950908@gmail.com>
2018-04-24 00:26:07 +0800

    fe add package redux scss

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// redux
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+import rootReducer from './reducers';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk, logger)),
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+  , document.getElementById('root'));
 registerServiceWorker();
---
+++ frontend/src/reducers/auth.js
@@ -0,0 +1,8 @@
+const auth = (state = {}, action = {}) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default auth;

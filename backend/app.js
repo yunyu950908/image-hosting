3dce42ae9950029337a3ace6dd819a708f8495fa
yunyu950908 <yunyu950908@gmail.com>
2018-04-30 09:26:20 +0800

    be update entry file

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,12 @@
-const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+require('./services/mongoose_service');
+const { appLogger } = require('./utils/loggers/loggerGenerator');
 const httpErrorHandler = require('./middlewares/http_error_handler');
 const errorHandler = require('./middlewares/error_handler');
+const NotFoundError = require('./errors/resource_not_found_error');
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
 
 const app = express();
 
@@ -14,33 +14,29 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // error handler
+app.use((req, res, next) => {
+  if (!req.headersSent) {
+    next(new NotFoundError(req.method, req.path));
+  }
+});
 app.use(httpErrorHandler());
 app.use(errorHandler());
 
-// catch 404 and forward to error handler
-// app.use((req, res, next) => {
-//   next(createError(404));
-// });
+process.on('uncaughtException', (err) => {
+  appLogger.error('uncaught exception error\n', err);
+});
 
-// error handler
-// app.use((err, req, res) => {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-//
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
+process.on('unhandledRejection', (reason, p) => {
+  appLogger.error('unhandled rejection error\n', { reason, p });
+});
+
 
 module.exports = app;

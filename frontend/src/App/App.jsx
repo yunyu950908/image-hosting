cde55863bd535b1235b14e90c6d37a582b6f45c0
yunyu950908 <yunyu950908@gmail.com>
2018-05-17 04:29:23 +0800

    fe add initial leancloud when app.jsx mounted

--- frontend/src/App/App.jsx
+++ frontend/src/App/App.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { replace } from 'react-router-redux';
-import { message } from 'antd';
 import './App.css';
 
 import { initLeancloud } from '../utils/leancloud';
@@ -12,14 +12,29 @@ import MainDOM from '../components/main_dom';
 import FooterDOM from '../components/footer_dom';
 
 class App extends Component {
+  static propTypes = {
+    userState: PropTypes.shape({
+      email: PropTypes.string,
+      hostSetting: PropTypes.shape({}),
+    }),
+  };
+
+  static defaultProps = {
+    userState: {
+      email: '',
+      hostSetting: {},
+    },
+  };
+
   componentDidMount() {
-    // try {
-    //   const { email, hostSetting } = this.props.userState;
-    //   const { leancloud } = hostSetting;
-    //   const { APP_ID, APP_KEY } = leancloud;
-    //   if (APP_ID && APP_KEY && email) initLeancloud(APP_ID, APP_KEY, email);
-    // } catch (e) {
-    // }
+    try {
+      const { email, hostSetting } = this.props.userState;
+      const { leancloud } = hostSetting;
+      const { APP_ID, APP_KEY } = leancloud;
+      if (APP_ID && APP_KEY && email) initLeancloud(APP_ID, APP_KEY, email);
+    } catch (e) {
+      console.error(e);
+    }
   }
 
   render() {
